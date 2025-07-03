8a317da7666c3e80331b31215f3d7e251d0409fa
Aman Kumar <amankumar8993kumar@gmail.com>
2025-07-03 13:29:57 +0530

    Update index.js

---
+++ frontend test submission/index.js
@@ -0,0 +1,13 @@
+import react from ' react';
+import reactDOM from 'react-dom/client';
+import app from './app';
+import cssbaseline from '@mui/material/cssbaseline';
+
+
+const root = reactDOM.crateroot document.getelementById('root'));
+root.render(
+  <react.strcitmode>
+  <cssbaseline/>
+  </app>
+  </react.strictmode>
+}
